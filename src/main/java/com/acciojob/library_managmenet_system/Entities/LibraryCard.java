c8c6c7ad54558af7e1a3fd289fa7b8ed5021bb2e
saifuddin khan <109955734+khansaif1023@users.noreply.github.com>
2024-03-05 14:55:46 +0530

    libraryCard Added

--- src/main/java/com/acciojob/library_managmenet_system/Entities/LibraryCard.java
+++ src/main/java/com/acciojob/library_managmenet_system/Entities/LibraryCard.java
@@ -2,7 +2,12 @@ package com.acciojob.library_managmenet_system.Entities;
 
 import com.acciojob.library_managmenet_system.CardStatus;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name="card_info")
@@ -16,6 +21,6 @@ public class LibraryCard {
     private int cardNo;
     private CardStatus cardStatus;
     private int noOfBooksIssued;
-    private Data validity;
+    private LocalDate vilidity;
 
 }
